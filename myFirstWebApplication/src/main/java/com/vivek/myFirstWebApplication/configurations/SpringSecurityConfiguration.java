9f5a084a06cd07b22cbde6ff40b4c80e49da3c3d
viveksoni100 <viveksoni100@gmail.com>
2023-04-13 01:11:49 +0530

    csrf and frames are disabled from securityFilterChain

--- myFirstWebApplication/src/main/java/com/vivek/myFirstWebApplication/configurations/SpringSecurityConfiguration.java
+++ myFirstWebApplication/src/main/java/com/vivek/myFirstWebApplication/configurations/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.vivek.myFirstWebApplication.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -38,4 +41,24 @@ public class SpringSecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /**
+     *
+     * csrf will prevent third party urls such as h2-console or swagger-ui.html
+     * that's why we are disabling it from spring security context
+     */
+    //CSRF/cross site request forgery disable
+    //frame
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
 }
