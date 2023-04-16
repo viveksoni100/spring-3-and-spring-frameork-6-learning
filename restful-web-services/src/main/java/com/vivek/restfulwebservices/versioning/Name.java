7901187ce95e1a61cc9e5576d8ae5c20204ed2df
viveksoni100 <viveksoni100@gmail.com>
2023-04-16 19:02:18 +0530

    versioning added for the api

---
+++ restful-web-services/src/main/java/com/vivek/restfulwebservices/versioning/Name.java
@@ -0,0 +1,14 @@
+package com.vivek.restfulwebservices.versioning;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Name {
+
+    private String firstName;
+    private String lastName;
+}
---
+++ restful-web-services/src/main/java/com/vivek/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,14 @@
+package com.vivek.restfulwebservices.versioning;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PersonV1 {
+
+    private String name;
+
+}
