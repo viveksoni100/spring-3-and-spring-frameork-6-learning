3a762cc14a9dcc236b53d495f9ddf89dee7fa194
viveksoni100 <viveksoni100@gmail.com>
2023-04-16 18:46:48 +0530

    i18n is added with the support of gujarati language as well

--- restful-web-services/src/main/java/com/vivek/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/vivek/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.vivek.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -20,6 +28,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 
 }
