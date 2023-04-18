3f38e7435b9918f35a937ce6c460614800faae97
viveksoni100 <viveksoni100@gmail.com>
2023-04-18 17:50:25 +0530

    @JsonProperty annotation added

--- restful-web-services/src/main/java/com/vivek/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/vivek/restfulwebservices/user/User.java
@@ -1,6 +1,6 @@
 package com.vivek.restfulwebservices.user;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -17,6 +17,7 @@ public class User {
 
     private Integer id;
     @Size(min = 3, message = "name should have atleast 3 characters")
+    @JsonProperty("user_name")  // customized name
     private String name;
     @Past(message = "birthdate should be in the past")   // birthdate should be in the past
     private LocalDate birthDate;
