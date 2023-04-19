8b5974d456ff3afa012dfe1eab8a123414794953
viveksoni100 <viveksoni100@gmail.com>
2023-04-19 21:25:06 +0530

    basic learning of react is done including react component and the use of javascript in it

---
+++ frontend_todo_in_react/todo_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LearningComponent from './components/learning-example/LearningComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <LearningComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend_todo_in_react/todo_app/src/components/learning-example/LearningJavascript.jsx
@@ -0,0 +1,29 @@
+// kind of a class
+const person = {
+    name: 'Vivek Soni',
+    address: {
+        line1: 'Baker Street',
+        city: 'London',
+        country: 'UK'
+    },
+    // array
+    profiles: ['twitter', 'linkedin', 'instagram'],
+    // function
+    printProfile: () => {
+        person.profiles.map(
+            (profile) => console.log(profile)
+        )
+    }
+}
+
+
+export default function LearningJavaScript() {
+    return (
+        <div>
+            <div>{person.name}</div>
+            <div>{person.address.line1}</div>
+            <div>{person.profiles[1]}</div>
+            <div>{person.printProfile()}</div>
+        </div>
+    )
+}
