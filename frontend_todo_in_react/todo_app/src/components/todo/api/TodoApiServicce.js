7e86f4fc717292fa9b3768d129d3048f59f0e00f
viveksoni100 <viveksoni100@gmail.com>
2023-04-30 17:41:43 +0530

    .

--- frontend_todo_in_react/todo_app/src/components/todo/api/TodoApiServicce.js
+++ frontend_todo_in_react/todo_app/src/components/todo/api/TodoApiServicce.js
@@ -4,4 +4,5 @@ const apiClient = axios.create({
     baseURL: 'http://localhost:9292'
 });
 
-export const retrieveTodosByUser = (username) => apiClient.get(`/users/${username}/todos`);
+export const retrieveTodosByUserApi = (username) => apiClient.get(`/users/${username}/todos`);
+export const deleteTodosByUserAndIdApi = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`);
