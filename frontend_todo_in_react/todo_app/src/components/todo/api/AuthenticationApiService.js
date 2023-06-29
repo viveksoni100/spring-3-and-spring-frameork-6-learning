98f7832588c3040ebfc1fe8fca7a9e20b85a7cbb
viveksoni100 <viveksoni100@gmail.com>
2023-06-30 01:09:40 +0530

    jwt token implemented

--- frontend_todo_in_react/todo_app/src/components/todo/api/AuthenticationApiService.js
+++ frontend_todo_in_react/todo_app/src/components/todo/api/AuthenticationApiService.js
@@ -1,3 +1,7 @@
 import {apiClient} from "./ApiClient";
 
 export const executeBasicAuthenticationService = () => apiClient.get(`/basicauth`);
+export const executeJWTAuthenticationService = (username, password) => apiClient.post(`/authenticate`, {
+    username,
+    password
+});
--- frontend_todo_in_react/todo_app/src/components/todo/security/AuthContext.js
+++ frontend_todo_in_react/todo_app/src/components/todo/security/AuthContext.js
@@ -1,6 +1,7 @@
 import {createContext, useContext, useState} from "react";
 import {apiClient} from "../api/ApiClient";
-import {executeBasicAuthenticationService} from "../api/AuthenticationApiService";
+// import {executeBasicAuthenticationService} from "../api/AuthenticationApiService";
+import {executeJWTAuthenticationService} from "../api/AuthenticationApiService";
 
 export const AuthContext = createContext();
 
@@ -12,7 +13,8 @@ export default function AuthProvider({children}) {
     const [username, setUsername] = useState(null);
     const [token, setToken] = useState(null);
 
-    async function login(username, password) {
+    // login with basic authentication
+    /*async function login(username, password) {
 
         const baToken = "Basic " + window.btoa(username + ":" + password);
 
@@ -27,6 +29,34 @@ export default function AuthProvider({children}) {
                 apiClient.interceptors.request.use(
                     (config) => {
                         config.headers.Authorization = baToken;
+                        return config;
+                    }
+                );
+
+                return true
+            } else {
+                logout();
+                return false
+            }
+        } catch (err) {
+            logout();
+        }
+    }*/
+
+    async function login(username, password) {
+
+        try {
+            const res = await executeJWTAuthenticationService(username, password);
+
+            if (res.status === 200) {
+                const jwtToken = 'Bearer ' + res.data.token;
+                setAuthenticated(true);
+                setUsername(username);
+                setToken(jwtToken);
+
+                apiClient.interceptors.request.use(
+                    (config) => {
+                        config.headers.Authorization = jwtToken;
                         return config;
                     }
                 );
