f45b97317411490ad38a2a7e99becf0d85007bb9
viveksoni100 <viveksoni100@gmail.com>
2023-06-30 20:05:28 +0530

    mockito with annotation added

--- restful-web-services/src/test/java/com/vivek/restfulwebservices/mockito/SomeBusinessImplMockTest.java
+++ restful-web-services/src/test/java/com/vivek/restfulwebservices/mockito/SomeBusinessImplMockTest.java
@@ -1,24 +1,35 @@
 package com.vivek.restfulwebservices.mockito;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class) // without this annotation mocks ill give nullPointer
 public class SomeBusinessImplMockTest {
+
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl someBusiness;
 
     @Test
     void findTheGreatestFromAllData_basicScenario() {
-        DataServiceStub dataserviceMock = mock(DataServiceStub.class);
-        when(dataserviceMock.retrieveAllData())
+        when(dataServiceMock.retrieveAllData())
                 .thenReturn(new int[]{1,2,3});
-//        DataServiceStub serviceStub = new DataServiceStub();
-        SomeBusinessImpl business = new SomeBusinessImpl(dataserviceMock);
 
-        assertEquals(3, business.findTheGreatestFromAllData());
+        assertEquals(3, someBusiness.findTheGreatestFromAllData());
     }
 
+    /**
+     * without mockito annotations
+     */
     @Test
     void findTheGreatestFromAllData_oneValueScenario() {
         DataServiceStub dataserviceMock = mock(DataServiceStub.class);
