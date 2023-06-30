30c288034235f704e57ed701c62a98c8a9bbca59
viveksoni100 <viveksoni100@gmail.com>
2023-06-30 18:39:30 +0530

    JUnit added

---
+++ restful-web-services/src/main/java/com/vivek/restfulwebservices/junit/MyMath.java
@@ -0,0 +1,13 @@
+package com.vivek.restfulwebservices.junit;
+
+public class MyMath {
+
+    public int calculateSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+}
---
+++ restful-web-services/src/test/java/com/vivek/restfulwebservices/junit/MyMathTest.java
@@ -0,0 +1,16 @@
+package com.vivek.restfulwebservices.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyMathTest {
+
+    @Test
+    void test() {
+        MyMath math = new MyMath();
+        int result = math.calculateSum(new int[]{1,2,3});
+
+        assertEquals(6, result);
+    }
+}
