3cbecb684aff3f1a8663a12b0d13570f856b1528
viveksoni100 <viveksoni100@gmail.com>
2023-06-30 19:51:25 +0530

    unit test added with stubs

---
+++ restful-web-services/src/main/java/com/vivek/restfulwebservices/mockito/SomeBusinessImpl.java
@@ -0,0 +1,24 @@
+package com.vivek.restfulwebservices.mockito;
+
+public class SomeBusinessImpl {
+
+    private DataService dataService;
+
+    public SomeBusinessImpl(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    public int findTheGreatestFromAllData() {
+        int[] data = dataService.retrieveAllData();
+        int greatestValue = Integer.MIN_VALUE;
+        for (int value : data) {
+            if (value > greatestValue)
+                greatestValue = value;
+        }
+        return greatestValue;
+    }
+}
+
+interface DataService {
+    int[] retrieveAllData();
+}
---
+++ restful-web-services/src/test/java/com/vivek/restfulwebservices/mockito/SomeBusinessImplTest.java
@@ -0,0 +1,23 @@
+package com.vivek.restfulwebservices.mockito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SomeBusinessImplTest {
+
+    @Test
+    void findTheGreatestFromAllData_basicScenario() {
+        DataServiceStub serviceStub = new DataServiceStub();
+        SomeBusinessImpl business = new SomeBusinessImpl(serviceStub);
+        assertEquals(3, business.findTheGreatestFromAllData());
+    }
+}
+
+class DataServiceStub implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{1, 2, 3};
+    }
+}
