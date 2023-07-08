1e0cffb9530d637933a3742217e7f30f0623f6d7
viveksoni100 <viveksoni100@gmail.com>
2023-07-08 17:27:07 +0530

    aop learning is started

---
+++ lear-aop/src/main/java/com/vivek/learaop/LearAopApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.learaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearAopApplication.class, args);
+	}
+
+}
---
+++ lear-aop/src/test/java/com/vivek/learaop/LearAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.learaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
