19ea3524958ee5f2f83e4407c80e6ed6515f6930
viveksoni100 <viveksoni100@gmail.com>
2023-07-12 22:22:15 +0530

    maven project added

---
+++ learn-maven/src/test/java/com/vivek/learnmaven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.learnmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
